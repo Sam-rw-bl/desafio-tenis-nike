c766966566a2953c1ad07b27aea61c8451972db9
sam18moura <samantha.moura1803@gmail.com>
2024-12-18 16:19:54 -0300

    finalizando

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "sans":["Poppins "]
+      },
+      backgroundImage:{
+        "tenis": url('/img/bg.png')
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
